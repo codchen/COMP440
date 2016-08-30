db0ce07d23a3cb853d4758c20f2d8297af9a57f4
Adam Wang <sw33@rice.edu>
2016-08-30 10:30:00 -0500

    Add files via upload
    Code for part 3

--- assignment1/submission.py
+++ assignment1/submission.py
@@ -1,4 +1,4 @@
-import re, util
+import re, util, collections
 
 ############################################################
 # Problem 1a: UCS test case
@@ -8,7 +8,13 @@ import re, util
 # util.createSearchProblemFromString.
 def createUCSTestCase(n):
     # BEGIN_YOUR_CODE (around 5 lines of code expected)
-    raise Exception("Not implemented yet")
+    problemStr = ""
+    for i in range(n - 1):
+        problemStr += "0 " + str(i + 2) + " 1\n"
+    for i in range(n - 1):
+        problemStr += str(i + 2) + " 1 2\n"
+    problemStr += "0 1 2"
+    return util.createSearchProblemFromString("0", "1", problemStr)
     # END_YOUR_CODE
 
 ############################################################
@@ -24,7 +30,16 @@ def astarReduction(problem, heuristic):
         # Please refer to util.SearchProblem to see the functions you need to
         # overried.
         # BEGIN_YOUR_CODE (around 9 lines of code expected)
-        raise Exception("Not implemented yet")
+        def startState(self):
+            return problem.startState()
+        def isGoal(self, state):
+            return problem.isGoal(state)
+        def succAndCost(self, state):
+            succ = problem.succAndCost(state)
+            result = []
+            for (action, newState, cost) in succ:
+                result.append((action, newState, cost + heuristic(newState) - heuristic(state)))
+            return result
         # END_YOUR_CODE
     newProblem = NewSearchProblem()
     return newProblem
@@ -43,6 +58,7 @@ class AStarSearch(util.SearchAlgorithm):
         # Copy solution back
         self.actions = algorithm.actions
         if algorithm.totalCost != None:
+            self.totalCost = algorithm.totalCost
             self.totalCost = algorithm.totalCost + self.heuristic(problem.startState())
         else:
             self.totalCost = None
@@ -55,17 +71,17 @@ class DeliveryProblem(util.SearchProblem):
     # |scenario|: delivery specification.
     def __init__(self, scenario):
         self.scenario = scenario
-
     # Return the start state.
     def startState(self):
         # BEGIN_YOUR_CODE (around 1 line of code expected)
-        raise Exception("Not implemented yet")
+        # list of packages carrying, list of packages left
+        return (self.scenario.truckLocation, (-1, ) * self.scenario.numPackages)
         # END_YOUR_CODE
 
     # Return whether |state| is a goal state or not.
     def isGoal(self, state):
         # BEGIN_YOUR_CODE (around 2 lines of code expected)
-        raise Exception("Not implemented yet")
+        return state[0] == self.scenario.truckLocation and state[1] == (1, ) * self.scenario.numPackages
         # END_YOUR_CODE
 
     # Return a list of (action, newState, cost) tuples corresponding to edges
@@ -75,7 +91,24 @@ class DeliveryProblem(util.SearchProblem):
         # at that location. In order for the simulation code to work, please use
         # the exact strings 'Pickup' and 'Dropoff' for those two actions.
         # BEGIN_YOUR_CODE (around 18 lines of code expected)
-        raise Exception("Not implemented yet")
+        neighbors = self.scenario.getNeighbors(state[0])
+        result = []
+        for action, loc in neighbors:
+            c = collections.Counter(state[1])
+            result.append((action, (loc, state[1]), 1 + c[0]))
+            if (state[0] in self.scenario.pickupLocations):
+                index = self.scenario.pickupLocations.index(state[0])
+                if (state[1][index] == -1):
+                    newPackageState = list(state[1])
+                    newPackageState[index] = 0
+                    result.append(('Pickup', (state[0], tuple(newPackageState)), 0))
+            if (state[0] in self.scenario.dropoffLocations):
+                index = self.scenario.dropoffLocations.index(state[0])
+                if (state[1][index] == 0):
+                    newPackageState = list(state[1])
+                    newPackageState[index] = 1
+                    result.append(('Dropoff', (state[0], tuple(newPackageState)), 0))
+        return result
         # END_YOUR_CODE
 
 ############################################################
@@ -88,7 +121,7 @@ class DeliveryProblem(util.SearchProblem):
 def createHeuristic1(scenario):
     def heuristic(state):
         # BEGIN_YOUR_CODE (around 2 lines of code expected)
-        raise Exception("Not implemented yet")
+        return abs(scenario.truckLocation[0] - state[0][0]) + abs(scenario.truckLocation[1] - state[0][1])
         # END_YOUR_CODE
     return heuristic
 
@@ -101,7 +134,19 @@ def createHeuristic1(scenario):
 def createHeuristic2(scenario, package):
     def heuristic(state):
         # BEGIN_YOUR_CODE (around 11 lines of code expected)
-        raise Exception("Not implemented yet")
+        pickup = scenario.pickupLocations[package]
+        dropoff = scenario.dropoffLocations[package]
+        if (state[1][package] == -1):
+            dist_pickup = abs(state[0][0] - pickup[0]) + abs(state[0][1] - pickup[1])
+            dist_dropoff = 2 * (abs(dropoff[0] - pickup[0]) + abs(dropoff[1] - pickup[1]))
+            dist_home = abs(dropoff[0] - scenario.truckLocation[0]) + abs(dropoff[1] - scenario.truckLocation[1])
+            return dist_pickup + dist_dropoff + dist_home
+        elif (state[1][package] == 0):
+            dist_dropoff = 2 * (abs(dropoff[0] - state[0][0]) + abs(dropoff[1] - state[0][1]))
+            dist_home = abs(dropoff[0] - scenario.truckLocation[0]) + abs(dropoff[1] - scenario.truckLocation[1])
+            return dist_dropoff + dist_home
+        else:
+            return abs(state[0][0] - scenario.truckLocation[0]) + abs(state[0][1] - scenario.truckLocation[1])
         # END_YOUR_CODE
     return heuristic
 
@@ -115,5 +160,7 @@ def createHeuristic2(scenario, package):
 # createHeuristic2.
 def createHeuristic3(scenario):
     # BEGIN_YOUR_CODE (around 5 lines of code expected)
-    raise Exception("Not implemented yet")
+    def heuristic(state):
+        return max([createHeuristic2(scenario, i)(state) for i in range(scenario.numPackages)])
+    return heuristic
     # END_YOUR_CODE
